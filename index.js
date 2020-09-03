94a9b0a0fab93d7a9039d1bbb6bf48d4f7856e90
Paul Nodet <paul.nodet@gmail.com>
2020-09-03 14:01:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const request = require("request");
+const fetch = require('isomorphic-fetch');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+(async () => {
+  const response = await fetch(
+    "https://academy.planningdirty.com/courses/roadmap-for-a-career-to-strategy-leadership-live-webinar-london"
+  );
+  const text = await response.text();
+  const dom = await new JSDOM(text);
+  const h1 = dom.window.document.querySelector("h1").textContent;
+  console.log(h1);
+
+  const WistiaClass = dom.window.document.querySelector("div.wistia_embed")
+    .id;
+  console.log(WistiaClass);
+
+  const 
+})();
