541d7beb20c083830e54286d39746e1cd4394887
Paul Nodet <paul.nodet@gmail.com>
2020-09-26 16:08:37 +0200

    tons of code

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 
+// for each linl in folder pages do blablabla …
+
 var text = fs.readFileSync(
   "/Users/pnodet/Documents/GitHub/wistia-downloader/pages/link1.html",
   "utf8"
 );
 
-var myRegexp = /wvideo=(..........)/;
+var myRegexp = /wvideo=([\s\S]*?)"></;
 var match = myRegexp.exec(text);
 //console.log(match[1]);
 
@@ -28,6 +30,7 @@ scrape(options)
     console.log("An error ocurred", err);
   });
 
+// make it async
 var videoFile = fs.readFileSync(
   "/Users/pnodet/Documents/GitHub/wistia-downloader/videoFiles/file_" +
     match[1] +
@@ -35,4 +38,22 @@ var videoFile = fs.readFileSync(
   "utf8"
 );
 
-console.log(videoFile);
+var binRegex = /url":"([\s\S]*?)","/;
+var binLink = binRegex.exec(videoFile);
+
+console.log(binLink[1])
+
+var boaRegex = /https:\/\/(.*)/;
+var httpLink = boaRegex.exec(binLink[1]);
+
+const http = require("http");
+
+const file = fs.createWriteStream("video"+match[1]+".mp4");
+const request = http.get("http://"+httpLink, function (response) {
+  response.pipe(file);
+});
+
+var opn = require("opn");
+
+// opens the url in the default browser
+opn(binLink);
