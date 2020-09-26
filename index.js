0a0421e1a92f9c85f5d9b78c6dd465d3a91710f1
Paul Nodet <paul.nodet@gmail.com>
2020-09-26 18:35:18 +0200

    yay

--- index.js
+++ index.js
@@ -1,55 +1,43 @@
 const fs = require("fs");
+const scrape = require("website-scraper");
+const http = require("http");
+
 
 // TODO for each link in folder pages do blablabla …
-var text = fs.readFileSync(
-  "/Users/pnodet/Documents/GitHub/wistia-downloader/pages/link1.html",
-  "utf8"
-);
+
+var text = fs.readFileSync("./pages/00.html", "utf8");
 
 var myRegexp = /wvideo=([\s\S]*?)"></;
 var match = myRegexp.exec(text);
 
-var newLink = "https://fast.wistia.net/embed/iframe/"+match[1]+"?videoFoam=true";
-
-const scrape = require("website-scraper");
+var newLink =
+  "https://fast.wistia.net/embed/iframe/" + match[1] + "?videoFoam=true";
 
 let options = {
   urls: [newLink],
-  directory: "./videoFiles/file_"+match[1],
+  directory: "./videoFiles/file_" + match[1],
 };
 
 scrape(options)
   .then((result) => {
     console.log("Website succesfully downloaded");
+
+    var videoFile = fs.readFileSync(
+      "./videoFiles/file_" + match[1] + "/index.html",
+      "utf8"
+    );
+
+    // regex the link in the page
+    var binRegex = /url":"([\s\S]*?)","/;
+    var binLink = binRegex.exec(videoFile);
+    let stringLink = binLink[1].toString();
+    console.log(stringLink);
+    
+    const file = fs.createWriteStream(match[1] + ".mp4");
+    const request = http.get(stringLink, function (response) {
+      response.pipe(file);
+    });
   })
   .catch((err) => {
     console.log("An error ocurred", err);
   });
-
-// TODO make it async
-var videoFile = fs.readFileSync(
-  "/Users/pnodet/Documents/GitHub/wistia-downloader/videoFiles/file_" +
-    match[1] +
-    "/index.html",
-  "utf8"
-);
-
-// regex the link in the page
-var binRegex = /url":"([\s\S]*?)","/;
-var binLink = binRegex.exec(videoFile);
-
-// print out the link
-console.log(binLink[1])
-
-// opens the url in the default browser
-var opn = require("opn");
-opn(binLink);
-
-// get rid of https
-var boaRegex = /https:\/\/(.*)/;
-var httpLink = boaRegex.exec(binLink[1]);
-
-// try to downlaod it via http
-//const http = require("http");
-//const file = fs.createWriteStream("video"+match[1]+".mp4");
-//const request = http.get("http://"+httpLink, function (response) { response.pipe(file); });
--- main.js
+++ main.js
@@ -1,42 +1,51 @@
 const fs = require("fs");
+const http = require("http")
 const scrape = require("website-scraper");
-const open = require("open");
 
-// TODO for each link in folder pages do blablabla …
-var text = fs.readFileSync(
-  "/Users/pnodet/Documents/GitHub/wistia-downloader/pages/link1.html",
-  "utf8"
-);
+var files = fs.readdirSync("./pages/");
+console.log(files);
 
-var myRegexp = /wvideo=([\s\S]*?)"></;
-var match = myRegexp.exec(text);
+for (i=0; i < files.length ; i++) {
+  var text = fs.readFileSync(
+    "./pages/"+files[i],
+    "utf8"
+  );
+  var myRegexp = /wvideo=([\s\S]*?)"></;
+  var match = myRegexp.exec(text);
+  console.log(match[1]);
 
-var newLink =
-  "https://fast.wistia.net/embed/iframe/" + match[1] + "?videoFoam=true";
+  var newLink =
+    "https://fast.wistia.net/embed/iframe/" + match[1] + "?videoFoam=true";
 
-let options = {
-  urls: [newLink],
-  directory: "./videoFiles/file_" + match[1],
-};
+  var options = {
+    urls: [newLink],
+    directory: "./videoFiles/file_" + match[1],
+  };
 
-scrape(options)
-  .then((result) => {
-    console.log("Website succesfully downloaded");
+  scrape(options)
+    .then((result) => {
+      console.log("Website " + match[1] + " succesfully downloaded");
 
-    var videoFile = fs.readFileSync(
-      "/Users/pnodet/Documents/GitHub/wistia-downloader/videoFiles/file_" +
-        match[1] +
-        "/index.html",
-      "utf8"
-    );
+      var videoFile = fs.readFileSync(
+        "./videoFiles/file_" +
+          match[1] +
+          "/index.html",
+        "utf8"
+      );
 
-    // regex the link in the page
-    var binRegex = /url":"([\s\S]*?)","/;
-    var binLink = binRegex.exec(videoFile);
+      // regex the link in the page
+      var binRegex = /url":"([\s\S]*?)","/;
+      var binLink = binRegex.exec(videoFile);
 
-    // print out the link
-    open(binLink[1]);
-  })
-  .catch((err) => {
-    console.log("An error ocurred", err);
+      // print out the link
+      console.log(binLink[1]);
+
+      const file = fs.createWriteStream("file.mp4");
+      const request = http.get(binLink[1], function (response) {
+        response.pipe(file);
+      });
+    })
+    .catch((err) => {
+      console.log("An error ocurred", err);
   });
+}
