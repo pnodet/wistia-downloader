1e6fdf7468fba00243a087a0486ebf1f5dcbc749
Paul Nodet <paul.nodet@gmail.com>
2020-11-05 10:31:15 +0100

    new file

---
+++ valentine.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const csv = require("csv-parser");
+const opn = require("opn");
+
+var readline = require("readline-sync");
+var inputFilePath = readline.question("Input file path : ");
+
+fs.createReadStream(inputFilePath)
+  .pipe(csv())
+  .on("data", function (data) {
+    try {
+      //perform the operation
+      opn(data.URL);
+    } catch (err) {
+      console.log(err);
+    }
+  })
+  .on("end", function () {
+    console.log("success");
+  });
