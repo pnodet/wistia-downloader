8d730ebe4cb3bc3dfa7b215ed8c3009f2d7f7432
Paul Nodet <paul.nodet@gmail.com>
2021-04-11 22:13:13 +0200

    Change require to import, es6

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
-const http = require("http");
-const scrape = require("website-scraper");
+import { readFileSync, createWriteStream } from "fs";
+import { get } from "http";
+import scrape from "website-scraper";
 
-var readline = require("readline-sync");
-var text = readline.question("Paste your link and press enter : ");
+import { question } from "readline-sync";
+var text = question("Paste your link and press enter : ");
 
 //var text = fs.readFileSync("./pages/00.html", "utf8");
 
@@ -22,7 +22,7 @@ scrape(options)
   .then((result) => {
     console.log("Website succesfully downloaded");
 
-    var videoFile = fs.readFileSync(
+    var videoFile = readFileSync(
       "./videoFiles/file_" + match[1] + "/index.html",
       "utf8"
     );
@@ -30,15 +30,15 @@ scrape(options)
     // regex the link in the page
     var binRegex = /url":"([\s\S]*?)","/;
     var binLink = binRegex.exec(videoFile);
-    
+
     var boaRegex = /https:\/\/(.*)/;
     var httpLink = boaRegex.exec(binLink[1]);
     var stringLink = "http://"+httpLink[1].toString();
     console.log(stringLink);
-    
+
     // download the video
-    const file = fs.createWriteStream(match[1] + ".mp4");
-    const request = http.get(stringLink, function (response) {
+    const file = createWriteStream(match[1] + ".mp4");
+    const request = get(stringLink, function (response) {
       response.pipe(file);
     });
 
