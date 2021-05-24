cd0981fcfb4ae8c7265074f4d88f5917d9d125d3
Paul Nodet <paul.nodet@gmail.com>
2021-05-24 22:13:13 +0200

    Use const instead of let

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-import { readFileSync, createWriteStream } from "fs";
-import { get } from "http";
-import scrape from "website-scraper";
+import {readFileSync, createWriteStream} from 'fs';
+import {get} from 'http';
+import scrape from 'website-scraper';
+import {question} from 'readline-sync';
 
-import { question } from "readline-sync";
-let text = question("Paste your link and press enter : ");
+const text = question('Paste your link and press enter : ');
 
 //let text = fs.readFileSync("./pages/00.html", "utf8");
 
-let myRegexp = /wvideo=([\s\S]*?)"></;
-let match = myRegexp.exec(text);
+const myRegexp = /wvideo=([\s\S]*?)"></;
+const match = myRegexp.exec(text);
 
-let newLink =
-  "https://fast.wistia.net/embed/iframe/" + match[1] + "?videoFoam=true";
+const newLink =
+	'https://fast.wistia.net/embed/iframe/' + match[1] + '?videoFoam=true';
 
-let options = {
-  urls: [newLink],
-  directory: "./videoFiles/file_" + match[1],
+const options = {
+	urls: [newLink],
+	directory: './videoFiles/file_' + match[1],
 };
 
 scrape(options)
-  .then((result) => {
-    console.log("Website succesfully downloaded");
+	.then((result) => {
+		console.log('Website succesfully downloaded');
 
-    let videoFile = readFileSync(
-      "./videoFiles/file_" + match[1] + "/index.html",
-      "utf8"
-    );
+		const videoFile = readFileSync(
+			'./videoFiles/file_' + match[1] + '/index.html',
+			'utf8',
+		);
 
-    // regex the link in the page
-    let binRegex = /url":"([\s\S]*?)","/;
-    let binLink = binRegex.exec(videoFile);
+		// regex the link in the page
+		const binRegex = /url":"([\s\S]*?)","/;
+		const binLink = binRegex.exec(videoFile);
 
-    let boaRegex = /https:\/\/(.*)/;
-    let httpLink = boaRegex.exec(binLink[1]);
-    let stringLink = "http://"+httpLink[1].toString();
-    console.log(stringLink);
+		const boaRegex = /https:\/\/(.*)/;
+		const httpLink = boaRegex.exec(binLink[1]);
+		const stringLink = 'http://' + httpLink[1].toString();
+		console.log(stringLink);
 
-    // download the video
-    const file = createWriteStream(match[1] + ".mp4");
-    const request = get(stringLink, function (response) {
-      response.pipe(file);
-    });
-
-  })
-  .catch((err) => {
-    console.log("An error ocurred", err);
-  });
+		// download the video
+		const file = createWriteStream(match[1] + '.mp4');
+		const request = get(stringLink, function (response) {
+			response.pipe(file);
+		});
+	})
+	.catch((err) => {
+		console.log('An error ocurred', err);
+	});
