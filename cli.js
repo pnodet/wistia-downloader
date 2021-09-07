122564e982dc38536a0857f6b40871bff4e412a7
Paul Nodet <paul.nodet@gmail.com>
2021-09-07 22:13:13 +0200

    Improve main js

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@ import {get} from 'node:http';
 import meow from 'meow';
 import scrape from 'website-scraper';
 import {question} from 'readline-sync';
+import logSymbols from 'log-symbols';
 
 const cli = meow(
 	`
@@ -16,27 +17,31 @@ const cli = meow(
 	},
 );
 let {input} = cli;
-if (!input) {
-	input = question('Paste your link and press enter : ');
+if (input.length === 0) {
+	console.error('Specify the url');
+	process.exit(1);
 }
 
 const myRegexp = /wvideo=([\s\S]*?)"></;
 const match = myRegexp.exec(input);
 
-const newLink =
-	'https://fast.wistia.net/embed/iframe/' + match[1] + '?videoFoam=true';
+const videoID = match[1];
+const videoIDChalk = chalk.bold(match[1]);
+const videoUrl = `https://fast.wistia.net/embed/iframe/${videoID}?videoFoam=true`;
 
 const options = {
-	urls: [newLink],
-	directory: './videoFiles/file_' + match[1],
+	urls: [videoUrl],
+	directory: `./videoFiles/file_${videoID}`,
 };
 
-const main = async () => {
+const spinner = ora(`Downloading website…`).start();
+
+(async () => {
 	await scrape(options);
-	console.log('Website succesfully downloaded');
+	spinner.text(`${logSymbols.success} Website succesfully downloaded`);
 
 	const videoFile = readFileSync(
-		'./videoFiles/file_' + match[1] + '/index.html',
+		`./videoFiles/file_${videoID}/index.html`,
 		'utf8',
 	);
 
@@ -50,10 +55,12 @@ const main = async () => {
 	console.log(stringLink);
 
 	// Download the video
-	const file = createWriteStream(match[1] + '.mp4');
+	const file = createWriteStream(`${videoID}.mp4`);
 	get(stringLink, (response) => {
 		response.pipe(file);
 	});
-};
-
-main();
+})().catch((error) => {
+	spinner.stop();
+	console.error(error);
+	process.exit(1);
+});
